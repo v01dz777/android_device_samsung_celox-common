d592b8231b6ad4ab2cf04019d57992b3914034c2
bryan2894 <bryan2894@gmail.com>
2014-08-06 23:57:10 -0400

    celox: remove zcache from cmdline
    Change-Id: I34fe7197aae02733ab7823ae1835d592adb2bba2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 
 # Kernel
 BOARD_KERNEL_BASE := 0x40400000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
