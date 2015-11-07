56a62b75427232e0a08c1aed65dc8c90487407f5
Steve Kondik <steve@cyngn.com>
2015-11-07 09:02:00 -0500

    celox: Build the kernel with gcc-4.9
    Change-Id: I49b7d4af6e2cccd70301c94b82106ec72c20a0f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,6 +24,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_cons
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Recovery
 BOARD_HAS_NO_MISC_PARTITION := true
