63955c4c771d0a9229bfbef072b14f0818b58e3c
HomuHomu <ecoco666@gmail.com>
2015-12-06 11:13:27 +0900

    celoxdcm: Add device filter
    Change-Id: I7a255ae67063f396b040e43feba4a6eac949d6a3

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hercules skyrocket t769,$(TARGET_DEVICE)),)
+ifneq ($(filter hercules skyrocket celoxdcm t769,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
