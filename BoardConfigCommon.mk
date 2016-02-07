f421d14847dd5e7bdc1ac12cd81abbc164ba962e
HomuHomu <ecoco666@gmail.com>
2016-02-07 22:10:41 +0900

    TWRP: update config
    Change-Id: Ic6c1587c9191870d741b451db5b3408f864c25f0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,12 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 
 # Kernel
 BOARD_KERNEL_BASE := 0x40400000
+TWRP_RECOVERY:= false
+ifeq ($(TWRP_RECOVERY),true)
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true androidboot.selinux=permissive
+else
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true
+endif
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
@@ -28,7 +33,11 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS := device/samsung/celox-common
+ifeq ($(TWRP_RECOVERY),true)
+TARGET_RECOVERY_FSTAB := device/samsung/celox-common/recovery/twrp.fstab
+else
 TARGET_RECOVERY_FSTAB := device/samsung/celox-common/rootdir/etc/fstab.qcom
+endif
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -36,23 +45,5 @@ BOARD_SUPPRESS_EMMC_WIPE := true
 
 # TWRP
 DEVICE_RESOLUTION := 480x800
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
-TW_EXCLUDE_SUPERSU := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_CPU_TEMP := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p28"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "noauto_da_alloc"
-TW_CRYPTO_FS_FLAGS := "0x00000006"
-TW_CRYPTO_KEY_LOC := "/efs/metadata"
-TW_MAX_BRIGHTNESS := 255
+TW_THEME := portrait_mdpi
+TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.196609/leds/lcd-backlight/brightness
